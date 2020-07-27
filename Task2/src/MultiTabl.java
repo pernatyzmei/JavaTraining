84dc3d46fcb5e4fc8317ccf45ad09cc555f2380f
yuchugunova <yuchugunova@phoenixit.ru>
2020-07-27 13:28:03 +0300

    Update MultiTabl.java

--- Task2/src/MultiTabl.java
+++ Task2/src/MultiTabl.java
@@ -3,10 +3,9 @@
  */
 public class MultiTabl {
     public static void main(String[] args) {
-        int x, y;
-        for(x=2;x<10;x++){
-            for (y = 2; y < 10; y++) {
-                System.out.print("\t" + x + " * " + y + " = " + (x * y));
+        for(int x=2;x<10;x++){
+            for (int y = 2; y < 10; y++) {
+                System.out.print("\t" + y + " * " + x + " = " + (x * y));
             }
             System.out.print("\n");
         }
