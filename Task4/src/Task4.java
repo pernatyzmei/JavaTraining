4f9bb2fbc6a9d92f42daff8ce8ca394bea49b1d1
yuchugunova <yuchugunova@phoenixit.ru>
2020-07-28 15:32:29 +0300

    правка на положительность

---
+++ Task4/src/Task4.java
@@ -0,0 +1,9 @@
+/**
+ * Created by yuchugunova on 27.07.2020.
+ */
+public class Task4 {
+    public static void main(String[] args) {
+        int a = -25;
+        System.out.println(a/10);
+    }
+}
